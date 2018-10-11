3e1d555883fe9cc92b7978b6b9f56de0e7931800
thma <thma32@web.de>
2018-10-11 22:40:32 +0200

    using monoid api (WIP)

--- src/Composite.hs
+++ src/Composite.hs
@@ -1,33 +1,45 @@
 module Composite where
 
-type Test = String
+type TestCase = String
 
-data TestComponent = 
-    Empty
-  | TestLeaf Test
-  | TestComposite (String, [TestComponent]) deriving (Show)
+data TestComponent = Test TestCase
+                   | TestSuite (String, [TestComponent]) deriving (Show)
+
 
 instance Semigroup TestComponent where
     (<>) = addTest
 
 instance Monoid TestComponent where
-    mempty = Empty
+    mempty = Test ""
 
-performAllTests :: TestComponent -> [String]
-performAllTests Empty = []
-performAllTests (TestLeaf t) = [t]
-performAllTests (TestComposite (title , c:cs)) = [title ++ " ["] ++ performAllTests c ++ concatMap performAllTests cs ++ ["]"]
+runTest :: TestComponent -> IO ()
+runTest = performAllTests 0 
+
+performAllTests :: Int -> TestComponent -> IO ()
+performAllTests _ (Test "") = return ()
+performAllTests n (Test t)  = putStrLn $ (replicate n ' ') ++ t ++ " ...operational"
+performAllTests n (TestSuite (title , list)) = do 
+    putStrLn $ (replicate n ' ') ++ title
+    mapM_ (performAllTests (n+2)) list
 
 addTest :: TestComponent -> TestComponent -> TestComponent
-addTest Empty t = t
-addTest t1@(TestLeaf _) t2 = TestComposite ("autonamed", [t1,t2])
-addTest (TestComposite (title, list)) t = TestComposite (title, list ++ [t])
+addTest (Test "") t = t
+addTest t (Test "") = t
+addTest t1@(Test _) t2 = TestSuite ("TestSuite", [t1,t2])
+addTest (TestSuite (title, list)) t = TestSuite (title, list ++ [t])
 
 compositeDemo = do
     putStrLn "Composite -> Monoid"
-    let t1 = TestLeaf "Fluxcapacitator working"
-    let t2 = TestLeaf "Warp Kernel operating"
-    let t3 = TestComposite ("Simple", [t1,t2])
-    let t4 = TestComposite ("Bigger", [t3,t1,t2])
-    let t5 = addTest t3 t4
-    print $ performAllTests t5
+    let t1 = Test "Flux capacitator"
+    let t2 = Test "Warp drive"
+    let t3 = Test "Ansible"
+    let t4 = Test "Tractor beam"
+    let t5 = Test "Tricorder"
+    let ts1 = TestSuite ("Ship", [t1,t2])
+    let ts2 = TestSuite ("Fleet", [t3,t4,t5])
+    let ts3 = t3 <> t4
+    let ts4 = mconcat [t1,t2,ts1,ts2]
+    runTest ts3
+    runTest $ (t1 <> t2) <> t3
+    --runTest $ t1 <> t2 <> t3
+    --runTest $ mconcat [ts1, ts2]
