80ab867793da05725d97e9f1efac3da2fecd430d
thma <thma32@web.de>
2018-10-31 22:41:44 +0100

    worked on iterator example

--- src/Iterator.hs
+++ src/Iterator.hs
@@ -2,9 +2,9 @@ module Iterator where
 import Singleton (Exp (..))
 import Visitor
 import Data.Functor.Const
-import Data.Monoid (Sum (..), getSum)
-import Control.Monad.State.Lazy
-import Control.Applicative
+import Data.Monoid (Sum (..), getSum)   -- for Sum
+import Control.Monad.State.Lazy         -- for State
+import Control.Applicative              -- for WrappedMonad
 
 instance Functor Exp where
     fmap f (Var x)       = Var x
@@ -18,10 +18,24 @@ instance Traversable Exp where
     traverse g (Add x y) = Add <$> traverse g x <*> traverse g y
     traverse g (Mul x y) = Mul <$> traverse g x <*> traverse g y
 
-str :: [Char]
-str = "hello \n nice \t and \n busy world"
+-- the applicative product adventure
+
+data Prod m n a = Prod {pfst:: m a, psnd:: n a} deriving (Show)
+
+instance (Functor m, Functor n) => Functor (Prod m n) where    
+    fmap f (Prod m n) = Prod (fmap f m) (fmap f n)
+
+instance (Applicative m, Applicative n) => Applicative (Prod m n) where
+    pure x = Prod (pure x) (pure x)
+    mf <*> mx = Prod (pfst mf <*> pfst mx) (psnd mf <*> psnd mx)
+
+-- Functor Product
+x :: (Functor m, Functor n) => (a -> m b) -> (a -> n b) -> (a -> Prod m n b)
+(f `x` g) y = Prod (f y) (g y) 
+
 
 type Count = Const (Sum Integer)
+
 count :: a -> Count b
 count _ = Const 1
 
@@ -31,50 +45,50 @@ cciBody = count
 cci :: String -> Count [a]
 cci = traverse cciBody
 
+
 lciBody :: Char -> Count a
-lciBody c = Const (if c == '\n' then 1 else 0)
---lciBody c = Const Sum $ test (c == '\n')
+lciBody c = Const (Sum $ test (c == '\n'))
+
+test :: Bool -> Integer
+test b = if b then 1 else 0
 
 lci :: String -> Count [a]
 lci = traverse lciBody
+
+clci :: String -> Prod Count Count [a]
+clci = traverse (cciBody `x` lciBody)
+
+{--
+wciBody :: Char -> (WrappedMonad (Prod (State Bool) Count)) a
+wciBody c =  pure $ state (updateState c) where
+    updateState :: Char -> Bool -> (Integer, Bool)
+    updateState c w = let s = c /= ' ' in (test (not(w && s)), s)
+
+wci :: String -> (WrappedMonad (Prod (State Bool) Count)) [a]
+wci = traverse wciBody
+
+clwci :: String -> (Prod (Prod Count Count) (WrappedMonad (Prod (State Bool) Count))) [a]
+clwci = traverse (cciBody `x` lciBody `x` wciBody)
+
+--}
+
+str :: [Char]
+str = "hello \n nice \t and \n busy world"
+
+
+iteratorDemo = do
+    putStrLn "Iterator -> Traversable"
+    let exp = Mul (Add (Val 3) (Val 1)) 
+                (Mul (Val 2) (Var "pi"))
+        env = [("pi", pi)]
+    print $ traverse (\x c -> if even x then [x] else [2*x]) exp 0
+    print $ clci str
+    --print $ clwci str
+                            
 
 wcmBody :: Char -> State (Integer, Bool) Char
 wcmBody c = let s =  c /= ' ' in do
                 (n, w) <- get
                 put (n+test(not(w && s)), s)
                 return c
-
-{-               
-wciBody :: Char -> (WrappedMonad (Prod (State Bool) Count)) a
-wciBody c =  pure (state (updateState c)) where
-    updateState :: Char -> Bool -> (Integer, Bool)
-    updateState c w = let s = c /= ' ' in (if not(w && s) then 1 else 0, s)
--}
---wci = traverse wciBody
-
-test :: Bool -> Integer
-test b = if b then 1 else 0
-
-data Prod m n a = Prod {pfst:: m a, psnd:: n a} deriving (Show)
-
-x :: (Functor m, Functor n) => (a -> m b) -> (a -> n b) -> (a -> Prod m n b)
-(f `x` g) y = Prod (f y) (g y) 
-
-instance (Functor m, Functor n) => Functor (Prod m n) where    
-  fmap f (Prod m n) = Prod (fmap f m) (fmap f n)
-
-instance (Applicative m, Applicative n) => Applicative (Prod m n) where
-    pure x = Prod (pure x) (pure x)
-    mf <*> mx = Prod (pfst mf <*> pfst mx) (psnd mf <*> psnd mx)
-
-clci :: String -> Prod Count Count [a]
-clci = traverse (cciBody `x` lciBody)
-
-iteratorDemo = do
-    putStrLn "Iterator -> Traversable"
-    let exp = Mul (Add (Val 3) (Val 1)) 
-                  (Mul (Val 2) (Var "pi"))
-        env = [("pi", pi)]
-    print $ traverse (\x c -> if even x then [x] else [2*x]) exp 0
-    print $ clci str
-                            
+    
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,4 @@
 module Main where
-import Data.Semigroup  
 import Strategy  
 import Singleton
 import Pipeline
