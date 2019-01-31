a59d7090ce3783f5af8e76810b239eafd376ae17
Mahler, Thomas <thomas.mahler@ista.com>
2019-01-31 16:55:48 +0100

    update

--- src/Combinators.hs
+++ src/Combinators.hs
@@ -35,10 +35,16 @@ data Term =
   | Apply (Term, Term)  -- an application
   deriving (Show)
 
--- "(%x y.ADD x y) 3 4"
--- "(%x.ADD x 4) 3"
+-- "(\x.ADD x 4) 3"
 sumTerm1 = Apply (Abs ("x", Apply (Op "ADD", Apply(Bound "x", Int 4))), Int 3)  
+-- "(\x y.ADD x y) 3 4"
 sumTerm2 = Apply (Apply (Abs ("x", Abs ("y", Apply (Op "ADD", Apply(Bound "x", Bound "y")))), Int 3) , Int 4) 
+
+--(\x.ADD 1 x)) 2
+sumTerm3 = Apply (Abs ("x", Apply (Op "ADD", Apply(Int 1, Bound "x"))), Int 2) 
+
+-- (\x.x) 4
+term4 = Apply (Abs("x", Bound "x"), Int 4)
 
 -- abstract a variable from a lambda term  
 abstract :: String -> Term -> Term  
@@ -54,7 +60,7 @@ data Snode =
  |Scomb Comb
  |Sapp (Snode, Snode) deriving (Show, Eq)
 
-data Comb = S | K | I | B | C | Y | CONS | HD | TL | PLUS | MINUS | TIMES | DIV | IF | EQL | AND | OR | NOT | DEF String deriving (Show, Eq)
+data Comb = S | K | I | B | C | Y | PLUS | MINUS | TIMES | DIV | IF | EQL | AND | OR | NOT | DEF String deriving (Show, Eq)
 
 data Value = 
   Integer Integer
@@ -71,17 +77,20 @@ c (Apply(a,b))  = Sapp(c a,c b)
 c (Abs(x,body)) = c (abstract x body);
 
 opt :: Snode -> Snode
-opt (Sapp(Sapp(Scomb S,Sapp(Scomb K,e)),Scomb I)) = e
+opt (Sapp(Sapp(Scomb S,Sapp(Scomb K,e)),Scomb I))           = e
 opt (Sapp(Sapp(Scomb S,Sapp(Scomb K,e1)),Sapp(Scomb K,e2))) = Sapp(Scomb K,Sapp(e1,e2))
-opt (Sapp(Sapp(Scomb S,Sapp(Scomb K,e1)),e2)) = Sapp(Sapp(Scomb B,e1),e2)
-opt (Sapp(Sapp(Scomb S,e1),Sapp(Scomb K,e2))) = Sapp(Sapp(Scomb C,e1),e2)
-opt (Sapp(e1,e2)) = Sapp(opt e1,opt e2)
-opt x = x;
+opt (Sapp(Sapp(Scomb S,Sapp(Scomb K,e1)),e2))               = Sapp(Sapp(Scomb B,e1),e2)
+opt (Sapp(Sapp(Scomb S,e1),Sapp(Scomb K,e2)))               = Sapp(Sapp(Scomb C,e1),e2)
+opt (Sapp(e1,e2))                                           = Sapp(opt e1,opt e2)
+opt x                                                       = x;
 
 ropt :: Snode -> Snode
-ropt x = 
-  let y = opt x;
-   in if y==x then x else ropt y
+ropt x = let y = opt x;
+          in if y==x then x else ropt y
+
+s1 = (Sapp(Sapp(Scomb S,Sapp(Scomb K,Satom (Integer 4))),Scomb I))
+s2 = (Sapp(Sapp(Scomb S,Sapp(Scomb K,Satom (Integer 5))),Sapp(Scomb K,Satom (Integer 3))))
+          
 
 mkComb "I" = I
 mkComb "K" = K
@@ -89,9 +98,6 @@ mkComb "S" = S
 mkComb "B" = B
 mkComb "C" = C
 mkComb "Y" = Y
-mkComb "CONS" = CONS
-mkComb "HD" = HD
-mkComb "TL" = TL
 mkComb "ADD" = PLUS
 mkComb "SUB" = MINUS
 mkComb "MUL" = TIMES
@@ -101,8 +107,27 @@ mkComb "EQ" = EQL
 mkComb "AND" = AND
 mkComb "OR" = OR
 mkComb "NOT" = NOT
---mkComb "PR" = PR
 mkComb str = DEF str;
+
+apply (I,(Sapp((_,x))))::_) = x
+apply (K,(Sapp((_,x)))::node::_) =
+  node := x
+  |apply (S,(ref(app((_,x),_,_)))::(ref(app((_,y),_,_)))
+  ::(node as (ref(app((_,z),_,_))))::_) =
+  node := app((ref(app((x,z),ref Eval,ref [])),
+  ref(app((y,z),ref Eval,ref []))),
+  ref Eval,ref [])
+  |apply (PLUS,ref(app((_,ref(atom(int x,_,_))),_,_))::(node as
+  ref(app((_,ref(atom(int y,_,_))),_,_)))::_) =
+  node := atom(int(x+y),ref Ready,ref [])
+  |apply (PLUS,(stack as ref(app((_,x),_,_))::
+  ref(app((_,y),_,_))::_)) =
+  node := atom(int((eval x)+(eval y)),ref Ready,ref [])
+
+spine :: Snode -> t -> (Snode, t)
+spine (Satom(a))  stack = (Satom(a),stack)
+spine (Scomb(c))  stack = (Scomb(c),stack)
+spine (Sapp(l,r)) stack = spine l stack
 
 combinatorDemo :: IO ()
 combinatorDemo = do
